9226cc5747ea022f6caea53f0136c18edadea148
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-07-01 22:35:09 +0600

    hosting

--- src/Components/Home/SocialMedia/SocialMedia.js
+++ src/Components/Home/SocialMedia/SocialMedia.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
